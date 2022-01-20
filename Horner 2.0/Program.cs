b4d3d05a0480353c935144aa043ab67bfba91dce
Jakub Vrtiška <1013@student.itgymnazium.czgit>
2022-01-20 18:57:16 +0100

    2nd commit

--- Horner 2.0/Program.cs
+++ Horner 2.0/Program.cs
@@ -9,7 +9,8 @@ namespace Horner_2._0
             var app3 = new Vietovy_vzorce();
             var app2 = new Polynom();
             var app4 = new vypis();
-            Console.WriteLine("Co chcete použít - Hornerovo schéma(1)" +'\n'+ "                 - Vietovy vzorce(2)" + '\n' + "                 - Vypsat polynom(kvadratická rovnice(3))");
+            var app5 = new Diskriminant();
+            Console.WriteLine("Co chcete použít - Hornerovo schéma(1)" +'\n'+ "                 - Vietovy vzorce(2) //bohužel nefunkční" + '\n' + "                 - Vypsat polynom(kvadratická rovnice(3))" + '\n' + "                 - Diskirminant(4)");
             string start = Console.ReadLine();
             if (start == "1")
             {
@@ -40,6 +41,18 @@ namespace Horner_2._0
                 Console.WriteLine("Jaký je člen č. 3");
                 app2.c = Console.ReadLine();
                 app4.Vypis(app2.a, app2.b, app2.c);
+            }
+            if (start == "4")
+            {
+                Console.WriteLine("Jaký je člen č. 1");
+                app2.a = Console.ReadLine();
+
+                Console.WriteLine("Jaký je člen č. 2");
+                app2.b = Console.ReadLine();
+
+                Console.WriteLine("Jaký je člen č. 3");
+                app2.c = Console.ReadLine();
+                app5.Run(app2.a, app2.b, app2.c);
             }
         }
     }
